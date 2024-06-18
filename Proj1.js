0b959e8da6f094541ce7af0388e3f25557f0e947
Vijay-creater <k.b.vijay61111@gmail.com>
2024-06-18 20:26:46 +0530

    Update Proj1.js

---
+++ Proj1.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+import fclass from './Fclass';
+
+ReactDOM.render(
+  fclass,
+  document.getElementById('root')
+);
